7379a1d1c1cf9c1733b295d8671bc998dd085d64
Cayle <39606715+Cayle@users.noreply.github.com>
2022-03-01 17:12:19 -0600

    Update settings.py

--- shoppr_site/shoppr_site/settings.py
+++ shoppr_site/shoppr_site/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ze34l#yye^$@z)$k_^ti^x040jvq=p-o8c-9cks%ygf%bbm9(n'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
